e257f14093e94367dc9f0ac2537c20ef8d357fac
Zike01 <zikekomakoma@gmail.com>
2025-08-02 15:40:40 +0100

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    content
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "tEsT";
+        let contents = "\
+Hello there
+fellow rustaceans
+this is a simple minigrep TEST";
+
+        assert_eq!(
+            vec!["this is a simple minigrep TEST"],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
